97d3348bd4269859bf97244c2e818e56fe431915
Eshwar Thath K R <eshwarthath@gmail.com>
2026-06-24 22:56:22 +0530

    Implement teleport home logic in CC_Teleport_Home.cpp

---
+++ CC_Teleport_Home.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int D,T;
+    cin>> D >> T;
+    
+    if(T>=D){
+        cout<<"0"<<endl;
+        
+    }else{
+        int a = D-T;
+        cout<<a<<endl;
+    }
+}
